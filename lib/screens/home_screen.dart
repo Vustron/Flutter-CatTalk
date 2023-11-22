da8148ef5fcd1bd2b13f5e4ec5cbb88dddf13f73
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2023-11-22 18:26:14 +0800

    Added Profile Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,7 @@ import '../main.dart';
 import '../controller/googleAuth.dart';
 import '../controller/api.dart';
 import 'auth/login_screen.dart';
+import 'profile_screen.dart';
 
 // Home Screen
 class HomeScreen extends StatefulWidget {
@@ -27,16 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final User? user = FirebaseAuth.instance.currentUser;
-    // Default values if the user is null
-    String name = 'Default Name';
-    String email = 'default@email.com';
-    String photoUrl = 'assets/images/vustron.png';
-    if (user != null) {
-      // Update values if the user is not null
-      name = user.displayName ?? name;
-      email = user.email ?? email;
-      photoUrl = user.photoURL ?? photoUrl;
-    }
+
     return Scaffold(
       appBar: AppBar(
         leading: const Icon(CupertinoIcons.home),
@@ -47,82 +39,17 @@ class _HomeScreenState extends State<HomeScreen> {
             icon: const Icon(Icons.search),
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ProfileScreen(user: list[0]),
+                  ));
+            },
             icon: const Icon(Icons.more_vert),
           ),
         ],
       ),
-      // body: Center(
-      //   child: Column(
-      //     children: [
-      //       const SizedBox(height: 10.0),
-      //       CircleAvatar(
-      //         backgroundImage: user != null
-      //             ? (photoUrl.startsWith('http') ||
-      //                     photoUrl.startsWith('https'))
-      //                 ? NetworkImage(photoUrl)
-      //                 : AssetImage(photoUrl) as ImageProvider<Object>?
-      //             : AssetImage(photoUrl),
-      //         radius: 50.0,
-      //       ),
-      //       const SizedBox(height: 10.0),
-      //       Text(
-      //         name,
-      //         style: const TextStyle(
-      //           color: Colors.amberAccent,
-      //           letterSpacing: 1.0,
-      //           fontSize: 12.0,
-      //           fontWeight: FontWeight.bold,
-      //         ),
-      //       ),
-      //       const SizedBox(height: 10.0),
-      //       Text(
-      //         email,
-      //         style: const TextStyle(
-      //           color: Colors.amberAccent,
-      //           letterSpacing: 1.0,
-      //           fontSize: 12.0,
-      //           fontWeight: FontWeight.bold,
-      //         ),
-      //       ),
-      //       SizedBox(
-      //         width: 121,
-      //         child: ElevatedButton.icon(
-      //           onPressed: () async {
-      //             try {
-      //               final provider = Provider.of<GoogleSignInProvider>(context,
-      //                   listen: false);
-      //               provider.googleLogout();
-      //               Navigator.pushReplacement(
-      //                   context,
-      //                   MaterialPageRoute(
-      //                     builder: (_) => const LoginScreen(),
-      //                   ));
-      //             } catch (error) {
-      //               print('Error during Google login: $error');
-      //             }
-      //           }, // Missing closing parenthesis here
-      //           icon: const Icon(
-      //             Icons.logout,
-      //             color: Colors.white,
-      //           ),
-      //           label: const Text(
-      //             'Logout',
-      //             style: TextStyle(
-      //               fontSize: 10,
-      //               color: Colors.white,
-      //             ),
-      //           ),
-      //           style: ButtonStyle(
-      //             backgroundColor: MaterialStateProperty.all(
-      //               Colors.red,
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 10),
         child: FloatingActionButton(
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,158 @@
+// ignore_for_file: avoid_unnecessary_containers, unused_import, sort_child_properties_last, avoid_print, unused_label, unused_local_variable, unnecessary_null_comparison, prefer_const_constructors, use_build_context_synchronously
+import 'dart:convert';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wechat/model/chat_user.dart';
+import 'package:wechat/utils/chatUserCard.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../main.dart';
+import '../controller/googleAuth.dart';
+import '../controller/api.dart';
+import 'auth/login_screen.dart';
+
+// Home Screen
+class ProfileScreen extends StatefulWidget {
+  final ChatUser user;
+  const ProfileScreen({super.key, required this.user});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final User? user = FirebaseAuth.instance.currentUser;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      // Logout Button
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton.extended(
+          onPressed: () async {
+            final provider =
+                Provider.of<GoogleSignInProvider>(context, listen: false);
+            await provider.googleLogout();
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => LoginScreen(),
+                ));
+          },
+          icon: const Icon(
+            Icons.logout,
+            color: Colors.white,
+          ),
+          label: const Text(
+            'Logout',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.redAccent,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: mq.width * .05),
+        child: Column(
+          children: [
+            // Space
+            SizedBox(width: mq.width, height: mq.height * .03),
+            // User Profile Picture
+            ClipRRect(
+              borderRadius: BorderRadius.circular(mq.height * .1),
+              child: CachedNetworkImage(
+                width: mq.height * .2,
+                height: mq.height * .2,
+                fit: BoxFit.fill,
+                imageUrl: widget.user.image,
+                placeholder: (context, url) {
+                  return CircularProgressIndicator();
+                },
+                errorWidget: (context, url, error) {
+                  return const CircleAvatar(
+                    child: Icon(Icons.person),
+                  );
+                },
+              ),
+            ),
+            // Space
+            SizedBox(height: mq.height * .03),
+            // User Email
+            Text(
+              widget.user.email,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 16,
+              ),
+            ),
+            // Space
+            SizedBox(height: mq.height * .05),
+            // User Name
+            TextFormField(
+              initialValue: widget.user.name,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: Colors.purple,
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                hintText: 'eg. John Doe',
+                labelText: 'Name',
+              ),
+            ),
+            // Space
+            SizedBox(height: mq.height * .02),
+            // User about
+            TextFormField(
+              initialValue: widget.user.about,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.info_outline,
+                  color: Colors.purple,
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                hintText: 'eg. Feeling Happy',
+                labelText: 'About',
+              ),
+            ),
+            // Space
+            SizedBox(height: mq.height * .04),
+            // Update Button
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(
+                Icons.edit,
+                size: 28,
+              ),
+              label: const Text(
+                'Update',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                minimumSize: Size(mq.width * .5, mq.height * .06),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
