59c769c4f51aae02828db09db030e4b3b959eee5
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2023-11-27 12:26:10 +0800

    Last Message And Time

--- lib/utils/chatUserCard.dart
+++ lib/utils/chatUserCard.dart
@@ -2,7 +2,10 @@
 import 'package:flutter/material.dart';
 import 'package:wechat/model/chat_user.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:wechat/utils/my_date_util.dart';
+import '../controller/api.dart';
 import '../main.dart';
+import '../model/message.dart';
 import '../screens/chat_screen.dart';
 
 class ChatUserCard extends StatefulWidget {
@@ -14,59 +17,91 @@ class ChatUserCard extends StatefulWidget {
 }
 
 class _ChatUserCardState extends State<ChatUserCard> {
+  // last message info (if null --> no message)
+  Message? _message;
+
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+      margin: EdgeInsets.symmetric(horizontal: mq.width * .04, vertical: 4),
       elevation: 0.5,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       child: InkWell(
-        onTap: () {
-          // Navigating to chat screen
-          Navigator.push(context,
-              MaterialPageRoute(builder: (_) => ChatScreen(user: widget.user)));
-        },
-        child: ListTile(
-          // User profile picture
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: CachedNetworkImage(
-              imageUrl: widget.user.image,
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => CircleAvatar(
-                child: Icon(Icons.person),
-              ),
-            ),
-          ),
-          // User name
-          title: Text(
-            widget.user.name,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          // Last message
-          subtitle: Text(
-            widget.user.about,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
-          // Last message time
-          trailing: Container(
-            width: 15,
-            height: 15,
-            decoration: BoxDecoration(
-              color: Colors.greenAccent.shade400,
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          // trailing: Text(
-          //   '12:00 PM',
-          //   style: TextStyle(
-          //     color: Colors.black,
-          //   ),
-        ),
-      ),
+          onTap: () {
+            // Navigating to chat screen
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (_) => ChatScreen(user: widget.user)));
+          },
+          child: StreamBuilder(
+            stream: API.getLastMessage(widget.user),
+            builder: (context, snapshot) {
+              final data = snapshot.data?.docs;
+              final list =
+                  data?.map((e) => Message.fromJson(e.data())).toList() ?? [];
+
+              if (list.isNotEmpty) {
+                _message = list[0];
+              }
+              return ListTile(
+                  // User profile picture
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(mq.height * 0.3),
+                    child: CachedNetworkImage(
+                      width: mq.height * .055,
+                      height: mq.height * .055,
+                      imageUrl: widget.user.image,
+                      placeholder: (context, url) =>
+                          CircularProgressIndicator(),
+                      errorWidget: (context, url, error) => CircleAvatar(
+                        child: Icon(Icons.person),
+                      ),
+                    ),
+                  ),
+                  // User name
+                  title: Text(
+                    widget.user.name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                    ),
+                  ),
+                  // Last message
+                  subtitle: Text(
+                    _message != null ? _message!.msg : widget.user.about,
+                    maxLines: 1,
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                  // Last message time
+                  trailing: _message == null
+                      ? null // Show nothing when no message is sent
+                      : _message!.read.isEmpty &&
+                              _message!.fromId != API.user.uid
+                          ?
+                          // Show for unread message
+                          Container(
+                              width: 15,
+                              height: 15,
+                              decoration: BoxDecoration(
+                                color: Colors.greenAccent.shade400,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            )
+                          :
+                          // Message sent time
+                          Text(
+                              MyDateUtil.getLastMessageTime(
+                                  context: context, time: _message!.sent),
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 10,
+                              ),
+                            ));
+            },
+          )),
     );
   }
 }
